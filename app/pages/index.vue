c881e7b4054a27881beae2ac7b6f04da3ea88f73
Navid Talebian <navidt81@gmail.com>
2025-10-03 16:45:26 +0330

    fix: split to two section npm chart and releases

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,4 @@
 <script setup lang="ts">
-const pkg = ref('')
-const loading = ref(false)
-const packages = ['vue', 'react', 'svelte', '@angular/core', 'solid-js', 'next', 'hono', 'fastify', 'nuxt', 'astro', 'supabase', '@adonisjs/core', '@strapi/strapi', '@nestjs/core','directus', 'lodash', 'date-fns', 'express', 'h3', 'nitropack', 'typescript', 'vite']
-// Get 4 random packages
-const randomPackages = useState('pkgs', () => {
-  const clone = packages.slice()
-  return clone.toSorted(() => 0.5 - Math.random()).slice(0, 4)
-})
-function goToPackage(pkg: string) {
-  loading.value = true
-  navigateTo(`/${pkg.trim()}`)
-}
 useSeoMeta({
   title: 'NPM Chart',
   ogTitle: 'NPM Chart',
@@ -20,39 +8,18 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold uppercase">
-      NPM Chart
-    </h1>
-    <p class="text-sm text-neutral-500 mt-1 text-center px-6 sm:px-0">
-      Search for a package to see its download stats over time.
-    </p>
-    <form class="flex gap-1 mt-4" @submit.prevent="goToPackage(pkg)">
-      <UInput
-        v-model="pkg"
-        size="xl"
-        placeholder="npm package"
-        autofocus
-        :ui="{
-          trailing: 'pr-1',
-          base: 'bg-white dark:bg-neutral-950'
-        }">
-        <template #trailing>
-          <UButton type="submit" icon="i-heroicons-magnifying-glass" :disabled="!pkg" :loading="loading" aria-label="Search" />
-        </template>
-      </UInput>
-    </form>
-    <div class="flex flex-wrap justify-center gap-1 mt-3">
-      <UButton
-        v-for="pkg in randomPackages"
-        :key="pkg"
-        :to="`/${pkg}`"
-        @click="goToPackage(pkg)"
-        color="neutral"
-        variant="outline"
-        :label="pkg"
-        size="sm"
-      />
+  <div class="grid grid-cols-2 divide-amber-500 divide-x-2 h-screen w-full justify-center">
+    <NPMWelcome />
+    <div class="flex flex-col items-center justify-center h-screen">
+      <h1 class="text-4xl font-bold uppercase">
+        Releases
+      </h1>
+      <p class="text-sm text-neutral-500 mt-1 text-center px-6 sm:px-0">
+        Show releases npm packages
+      </p>
+      <UButton class="mt-2" to="/releasess">
+        show releases
+      </UButton>
     </div>
   </div>
 </template>
---
+++ app/pages/releases.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="">
+    <input
+      type="text"
+      name=""
+    />
+  </div>
+</template>
